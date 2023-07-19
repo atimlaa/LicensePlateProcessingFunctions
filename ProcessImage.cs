b38f6715868b2466d554701aa8af5f5e438667c9
atimlaa <aryan.timla@huskers.unl.edu>
2023-07-19 10:12:06 -0500

    initial commit

---
+++ ProcessImage.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Azure.Messaging.EventGrid;
+using System.IO;
+
+namespace LicensePlateProcessingFunctions
+{
+    public static class ProcessImage
+    {
+        [FunctionName("ProcessImage")]
+        public static void Run([EventGridTrigger] EventGridEvent eventGridEvent
+                                , [Blob(blobPath: "{data.url}", access: FileAccess.Read,
+                                    Connection = "plateImagesStorageConnection")] Stream incomingPlateImageBlob
+                                , ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+            log.LogInformation($"File Length: {incomingPlateImageBlob.Length}");
+        }
+    }
+}
